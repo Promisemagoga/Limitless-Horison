c3cc5aed2e8dd5f6af3ca97d0653bbf9c4889fff
Promise Tshegofatso magoga <104593732+Promisemagoga@users.noreply.github.com>
2023-03-05 17:47:02 +0200

    Added files

---
+++ index.js
@@ -0,0 +1,22 @@
+//Get the button
+let mybutton = document.getElementById("myBtn");
+
+//when user scroolls down 20px from the top of the docoment, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+
+function scrollFunction(){
+    if(document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display ="none";
+    }
+}
+
+//when the user clicks thebutton,scroll to the top of the document
+function backTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
